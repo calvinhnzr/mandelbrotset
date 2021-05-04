4ac78743e900a733befbe198bb13dbb03116f676
calvinhnzr <52936430+calvinhnzr@users.noreply.github.com>
2021-05-04 14:47:43 +0200

    [TASK] refactoring and logic

--- src/components/math/ExponentSequence.js
+++ src/components/math/ExponentSequence.js
@@ -1,38 +1,75 @@
 import { useState } from "react"
 import styled from "styled-components"
 
-// remove all components except ExponentSequence
-// leave Styled components instead
-// only one Main component
-
-const StyledStartingPoint = styled.div`
+const StyledInputContainer = styled.div`
 	position: relative;
 	width: 5rem;
+	// exponent ^2
+	&::after {
+		content: "2";
+		position: absolute;
+		top: -0.8rem;
+		right: -1.5rem;
+		font-family: "Roboto";
+		font-size: 1.4rem;
+		font-weight: bold;
+		color: #437ef1;
+	}
+`
+
+const StyledInput = styled.input`
+	background-color: #363738;
+	border: none;
+	border-radius: 0.2rem;
+	height: auto;
+	width: 5rem;
+	padding: 0.5rem;
+	font-family: "Roboto";
+	font-size: 1.6rem;
+	color: white;
+`
+const StyledList = styled.ul`
+	margin-top: 2rem;
+	height: 100%;
+`
+
+const StyledListItem = styled.li`
+	width: fit-content;
+	margin-bottom: 2.5rem;
+	font-family: "Roboto";
+	font-size: 1.5rem;
+	color: white;
+	position: relative;
+	letter-spacing: 1px;
+	// exponent ^2
 	&::after {
 		position: absolute;
 		top: -0.8rem;
 		right: -1.5rem;
 		content: "2";
 		font-family: "Roboto";
-		font-size: 1.4rem;
+		font-size: 1.3rem;
 		font-weight: bold;
 		color: #437ef1;
 	}
-	input {
-		background-color: #363738;
-		border: none;
-		border-radius: 0.2rem;
-		height: auto;
-		width: 5rem;
-		padding: 0.5rem;
-		font-family: "Roboto";
-		font-size: 1.6rem;
-		color: white;
+	&:last-of-type {
+		&::after {
+			display: none;
+		}
 	}
 `
 
-const StartingPoint = () => {
+const StyledExponentSequence = styled.div`
+	margin-top: 2rem;
+	grid-row-start: 2;
+	grid-row-end: 6;
+	grid-column: 2 / 12;
+`
+
+const ExponentSequence = () => {
+	// handle input
 	const [num, setNum] = useState(9)
+
 	const handleChange = (e) => {
 		let num = e.target.value
 		let length = num.toString().length
@@ -42,53 +79,10 @@ const StartingPoint = () => {
 		}
 	}
 
-	return (
-		<StyledStartingPoint>
-			<input
-				type="number"
-				min="1"
-				max="9"
-				placeholder="x:"
-				value={num}
-				onChange={handleChange}
-			/>
-		</StyledStartingPoint>
-	)
-}
-
-const StyledSequence = styled.ul`
-	margin-top: 2rem;
-	height: 100%;
-	li {
-		width: fit-content;
-		margin-bottom: 2.5rem;
-		font-family: "Roboto";
-		font-size: 1.5rem;
-		color: white;
-		position: relative;
-		letter-spacing: 1px;
-		&::after {
-			position: absolute;
-			top: -0.8rem;
-			right: -1.5rem;
-			content: "2";
-			font-family: "Roboto";
-			font-size: 1.3rem;
-			font-weight: bold;
-			color: #437ef1;
-		}
-		&:last-of-type {
-			&::after {
-				display: none;
-			}
-		}
-	}
-`
-
-const Sequence = () => {
+	// calculate exponent
 	const myArr = []
 	const numOfIterations = 5
-	let myStartingPoint = 9
+	let myStartingPoint = num
 
 	for (let i = 0; i < numOfIterations; i++) {
 		myStartingPoint = Math.pow(myStartingPoint, 2)
@@ -96,31 +90,26 @@ const Sequence = () => {
 	}
 
 	return (
-		<StyledSequence>
-			{myArr.map((item, index) => (
-				<li
-					key={index}
-					style={{ marginLeft: 2 * (index + 2.5) + "rem" }}>
-					{item}
-				</li>
-			))}
-		</StyledSequence>
-	)
-}
-
-const StyledExponentSequence = styled.div`
-	/* outline: 1px solid white; */
-	margin-top: 2rem;
-	grid-row-start: 2;
-	grid-row-end: 6;
-	grid-column: 2 / 12;
-`
-
-const ExponentSequence = () => {
-	return (
 		<StyledExponentSequence>
-			<StartingPoint />
-			<Sequence />
+			<StyledInputContainer>
+				<StyledInput
+					type="number"
+					min="1"
+					max="9"
+					placeholder="x:"
+					value={num}
+					onChange={handleChange}
+				/>
+			</StyledInputContainer>
+			<StyledList>
+				{myArr.map((item, index) => (
+					<StyledListItem
+						key={index}
+						style={{ marginLeft: 2 * (index + 2.5) + "rem" }}>
+						{item}
+					</StyledListItem>
+				))}
+			</StyledList>
 		</StyledExponentSequence>
 	)
 }
