9b1829b8a5b78cd7a4083ad56f86977d2dd009b1
calvinhnzr <52936430+calvinhnzr@users.noreply.github.com>
2021-06-01 15:18:11 +0200

    [FEATURE] added gauss chapter

--- src/templates/Gauss.js
+++ src/templates/Gauss.js
@@ -1,41 +1,76 @@
-import { Line, Html, CubicBezierLine } from "@react-three/drei"
 import { useState } from "react"
-
-import Scene from "../components/three/Scene"
-import MyAxisX from "../components/three/MyAxisX"
-import MyAxisY from "../components/three/MyAxisY"
 import styled from "styled-components"
 
-const StyledNumAxis = styled.span`
-	font-family: "Roboto";
-	font-size: "1rem";
-	color: #ea5b89;
-`
-const Button = styled.button`
-	/* grid-column: 2 / 12; */
-	grid-column: 2 / 7;
-	grid-row: 7 / 8;
-	/* opacity: 0; */
-	z-index: 1;
-	&:last-of-type {
-		grid-column: 7 / 12;
-	}
-`
+import gauss1 from "../images/gauss1.png"
+import gauss2 from "../images/gauss2.png"
+import gauss3 from "../images/gauss3.png"
+import gauss4 from "../images/gauss4.png"
 
 const SubText = styled.p`
-	/* outline: 1px solid white; */
 	grid-column: 2 / 12;
 	grid-row: 8 / 12;
 	font-size: 1.2rem;
 	font-weight: 100;
 	line-height: 1.3;
+	@media only screen and (min-width: 960px) {
+		/* outline: 1px solid white; */
+		grid-column: 8 / 12;
+		grid-row: 3 / 11;
+		font-size: 1.5rem;
+		align-self: center;
+	}
 `
 
+const Show = styled.div`
+	/* outline: 1px solid white; */
+	position: relative;
+	grid-column: 2 /12;
+	grid-row: 3 / 8;
+	background-color: #191a1b;
+	border-radius: 0.5rem;
+	& > img {
+		object-fit: cover;
+		width: 100%;
+		height: 100%;
+		transform: translateY(1rem);
+	}
+	@media only screen and (min-width: 960px) {
+		grid-column: 2 /7;
+		grid-row: 3 / 11;
+		display: flex;
+		justify-content: center;
+		& > img {
+			/* outline: 1px solid white; */
+			/* transform: none; */
+			object-fit: fill;
+			width: auto;
+			height: 100%;
+		}
+	}
+`
 const Counter = styled.p`
-	grid-column: 10 / 12;
-	grid-row: 7 / 8;
+	position: absolute;
+	bottom: 1rem;
+	right: 1rem;
+	z-index: 10;
+`
 
-	z-index: 1;
+const Buttons = styled.div`
+	position: absolute;
+	height: 100%;
+	bottom: 0;
+	left: 0;
+	display: flex;
+	width: 100%;
+`
+
+const Button = styled.button`
+	opacity: 0;
+	height: 100%;
+	width: 50%;
+	&:last-of-type {
+		right: 0 !important;
+	}
 `
 
 const Gauss = () => {
@@ -54,138 +89,18 @@ const Gauss = () => {
 	const increment = () => (!(current >= max - 1) ? 1 : 0)
 	const decrement = () => (!(current <= 0) ? 1 : 0)
 
-	const Point = (props) => {
-		return (
-			<>
-				<Line
-					name="axis"
-					points={[
-						[props.x, 0.1, 0],
-						[props.x, -0.1, 0],
-					]}
-					color="#EA5B89"
-					lineWidth={5}
-				/>
-				<Html
-					center={true}
-					position={[props.x, -0.5, 0]}
-					distanceFactor={5}>
-					<StyledNumAxis>x</StyledNumAxis>
-				</Html>
-			</>
-		)
-	}
-
-	const HalfCircle = () => {
-		let num = (4 * (Math.sqrt(2) - 1)) / 3
-		let x = 1.5
-		num *= x
-		let y = 0.5
-		const z = 0
-		const myColor = "#ea5b89"
-		// multiply factor
-
-		return (
-			<>
-				<Line
-					points={[
-						[-1.5, 0.5, 0],
-						[-1.7, 0.7, 0],
-					]}
-					color="#EA5B89"
-					lineWidth={3}
-				/>
-				<Line
-					points={[
-						[-1.5, 0.5, 0],
-						[-1.3, 0.7, 0],
-					]}
-					color="#EA5B89"
-					lineWidth={3}
-				/>
-				<CubicBezierLine
-					// x y z
-					start={[-x, y, z]}
-					midA={[-x, num + y, z]}
-					midB={[-num, x + y, z]}
-					end={[0, x + y, z]}
-					lineWidth={3}
-					color={myColor}
-					dashed={false}
-					dashSize={1}
-					dashScale={10}
-				/>
-				<CubicBezierLine
-					// x y z
-					start={[0, x + y, z]}
-					midA={[num, x + y, z]}
-					midB={[x, num + y, z]}
-					end={[x, 0 + y, z]}
-					lineWidth={3}
-					color={myColor}
-					dashed={false}
-					dashSize={1}
-					dashScale={10}
-				/>
-			</>
-		)
-	}
-
-	const QuarterCircle = () => {
-		let num = (4 * (Math.sqrt(2) - 1)) / 3
-		let x = 1.5
-		num *= x
-		let y = 0
-		const z = 0
-		const myColor = "#ea5b89"
-		return (
-			<>
-				<CubicBezierLine
-					// x y z
-					start={[0, x + y, z]}
-					midA={[num, x + y, z]}
-					midB={[x, num + y, z]}
-					end={[x, 0 + y, z]}
-					lineWidth={5}
-					color={myColor}
-					dashed={false}
-					dashSize={1}
-					dashScale={10}
-				/>
-			</>
-		)
-	}
-
-	const myScene = [
-		<>
-			<Point x={-1.5} />
-			<Point x={1.5} />
-		</>,
-		<>
-			<HalfCircle position={(0, 1, 0)} />
-			<Point x={-1.5} />
-			<Point x={1.5} />
-		</>,
-		<>
-			<MyAxisY length={length} />
-			<Point x={1.5} />
-			<QuarterCircle />
-		</>,
-	]
+	const images = [gauss1, gauss2, gauss3, gauss4]
 
 	return (
 		<>
-			<Scene className="gauss" control>
-				<MyAxisX null length={length} />
-				{myScene[current - 1]}
-			</Scene>
-			<Counter>{current + 1} / 4</Counter>
-			<Button onClick={() => setcurrent(current - decrement())}>
-				Previous
-			</Button>
-			<Button onClick={() => setcurrent(current + increment())}>
-				Next
-			</Button>
+			<Show>
+				<img src={images[current]} />
+				<Counter>{current + 1} / 4</Counter>
+				<Buttons>
+					<Button onClick={() => setcurrent(current - decrement())} />
+					<Button onClick={() => setcurrent(current + increment())} />
+				</Buttons>
+			</Show>
 
 			<SubText>{subText[current]}</SubText>
 		</>
