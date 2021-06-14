95498f536bba50418c799de3825975618282dd02
calvinhnzr <52936430+calvinhnzr@users.noreply.github.com>
2021-06-14 16:51:19 +0200

    [BUGFIX] improved perfomance

--- src/components/three/DragableCircle.js
+++ src/components/three/DragableCircle.js
@@ -1,17 +1,23 @@
+import { useState, useEffect, useRef } from "react"
 import { Circle } from "@react-three/drei"
 import { useThree } from "@react-three/fiber"
 import { useDrag } from "react-use-gesture"
 
 const DragableCircle = (props) => {
-	const { viewport } = useThree()
-	const { width, height, factor } = viewport
-
 	let defaultX = 0.4
 	let defaultY = 0.4
 
+	const [boxPos, setBoxPos] = useState({
+		position: [defaultX, defaultY, 0],
+	})
+
+	const { viewport } = useThree()
+	const { width, height, factor } = viewport
+
 	const bind = useDrag(
 		({ offset: [x, y] }) =>
-			props.setBoxPos({ position: [x + defaultX, y + defaultY, 0] }),
+			setBoxPos({ position: [x + defaultX, y + defaultY, 0] }),
+
 		{
 			// bounds are expressed in canvas coordinates!
 			bounds: {
@@ -26,16 +32,24 @@ const DragableCircle = (props) => {
 	)
 
 	return (
-		<Circle
-			args={[0.05, 64]}
-			position={[
-				props.boxPos.position[0].toFixed(2),
-				props.boxPos.position[1].toFixed(2),
-				0,
-			]}
-			{...bind()}>
-			<meshBasicMaterial attach="material" color="#EA5B89" />
-		</Circle>
+		<>
+			<Circle
+				args={[0.05, 64]}
+				position={[boxPos.position[0], boxPos.position[1], 0]}
+				{...bind()}>
+				<meshBasicMaterial attach="material" color="#EA5B89" />
+			</Circle>
+			<Circle
+				args={[0.05, 64]}
+				position={[
+					boxPos.position[0] * boxPos.position[0] -
+						boxPos.position[1] * boxPos.position[1],
+					2 * (boxPos.position[0] * boxPos.position[1]),
+					0,
+				]}>
+				<meshBasicMaterial attach="material" color="white" />
+			</Circle>
+		</>
 	)
 }
 
--- src/templates/SquareComplex.js
+++ src/templates/SquareComplex.js
@@ -8,38 +8,32 @@ import DragableCircle from "../components/three/DragableCircle"
 import { useFrame } from "@react-three/fiber"
 
 const SquareComplex = () => {
-	let defaultX = 0.4
-	let defaultY = 0.4
+	// let boxPos = {
+	// 	position: [defaultX, defaultY, 0],
+	// }
 
-	const [boxPos, setBoxPos] = useState({
-		position: [defaultX, defaultY, 0],
-	})
-
-	const myCircle = useRef(0)
 	// X
 	// const [re, setRe] = useState()
 	// Y
 	// const [im, setIm] = useState()
 	// let re1 = re * re - im * im
 	// let im1 = 2 * (re * im)
-	let re1 =
-		boxPos.position[0] * boxPos.position[0] -
-		boxPos.position[1] * boxPos.position[1]
 
-	let im1 = 2 * (boxPos.position[0] * boxPos.position[1])
+	// let re1 =
+	// 	boxPos.position[0] * boxPos.position[0] -
+	// 	boxPos.position[1] * boxPos.position[1]
+
+	// let im1 = 2 * (boxPos.position[0] * boxPos.position[1])
+
+	// let re1 = boxPos.position[0] + 0.5
+	// let im1 = boxPos.position[1] + 0.5
 
 	return (
 		<>
 			<Scene control position={[0, 0, 5]}>
 				<Axis />
 				<Ring />
-				<DragableCircle boxPos={boxPos} setBoxPos={setBoxPos} />
-				<Circle
-					ref={myCircle}
-					args={[0.05, 64]}
-					position={[re1, im1, 0]}>
-					<meshBasicMaterial attach="material" color="white" />
-				</Circle>
+				<DragableCircle />
 			</Scene>
 		</>
 	)
